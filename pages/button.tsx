9f171acdf7ec726bef9677a2c23151cf60a91129
Mndraca <marizanovic3@gmail.com>
2024-09-16 14:20:10 -0400

    Button

--- pages/button.tsx
+++ pages/button.tsx
@@ -3,15 +3,10 @@
 import React, { FC } from "react";
 
 interface ButtonProps {
-  name: string;
   handleClick: (event: React.FormEvent) => void;
 }
 
-const Button: FC<ButtonProps> = ({ name, handleClick }) => {
-  const upperCapB = (name: string) => {
-    return name.slice(0, 1).toUpperCase() + name.slice(1);
-  };
-
+const Button: FC<ButtonProps> = ({ handleClick }) => {
   return (
     <>
       <button
@@ -19,7 +14,7 @@ const Button: FC<ButtonProps> = ({ name, handleClick }) => {
         className="border border-transparent bg-gradient-to-br from-rose-300 via-rose-400 to-rose-300 text-black w-full  ml-16 sm:w-1/2 rounded-full shadow-2xl shadow-rose-700 transition h-8 hover:opacity-75 mt-8 font-bold"
         onClick={handleClick}
       >
-        {upperCapB(name)}
+        Submit
       </button>
     </>
   );
